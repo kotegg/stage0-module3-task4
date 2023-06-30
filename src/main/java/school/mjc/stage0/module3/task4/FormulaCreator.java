52e56357d7fc6681b8b4bd1e6933f369a3921de2
kotegg <lolpol.kek123@gmail.com>
2023-06-30 22:49:21 +0300

    1

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -1,6 +1,15 @@
 package school.mjc.stage0.module3.task4;
 
+import java.util.Scanner;
+
 public class FormulaCreator {
     public static void main(String[] args) {
+        double a, b, c;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Input a, b: ");
+        a = in.nextDouble();
+        b = in.nextDouble();
+        c = (9*a*a-5*b+2+a-7) * ((a+b - 4*a*b)/2);
+        System.out.println(c);
     }
 }
--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int first = 99;
+        int second = 104;
+        int third = 97;
+        int fourth = 114;
+        char c = (char) first;
+        char h = (char) second;
+        char a = (char) third;
+        char r = (char) fourth;
+        System.out.print(c);
+        System.out.print(h);
+        System.out.print(a);
+        System.out.print(r);
     }
 }
