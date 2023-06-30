1beda922a17f8d11e0d84ea27553e930c3d422f6
kotegg <lolpol.kek123@gmail.com>
2023-06-30 22:54:37 +0300

    2

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -4,11 +4,7 @@ import java.util.Scanner;
 
 public class FormulaCreator {
     public static void main(String[] args) {
-        double a, b, c;
-        Scanner in = new Scanner(System.in);
-        System.out.println("Input a, b: ");
-        a = in.nextDouble();
-        b = in.nextDouble();
+        double a = 5, b = 3.2, c;
         c = (9*a*a-5*b+2+a-7) * ((a+b - 4*a*b)/2);
         System.out.println(c);
     }
